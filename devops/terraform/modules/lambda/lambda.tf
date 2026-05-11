f8113f9dd345de16cfb1d1039e07430b9ce12e4c
Nathan Mittelette <mittelette.nathan@gmail.com>
2026-05-11 13:31:49 +0200

    feat: upgrade deploy env + deploy arch + deploy directories

--- devops/terraform/modules/lambda/lambda.tf
+++ devops/terraform/modules/lambda/lambda.tf
@@ -6,11 +6,11 @@ resource "aws_lambda_function" "lambda" {
   handler       = var.lambda.handler
   memory_size   = var.lambda.memory
   timeout       = var.lambda.timeout
-  architectures = ["arm64"]
+  architectures = ["x86_64"]
   tracing_config {
     mode = "Active"
   }
-  layers           = ["arn:aws:lambda:eu-west-1:580247275435:layer:LambdaInsightsExtension-Arm64:20"]
+  layers           = ["arn:aws:lambda:eu-west-1:580247275435:layer:LambdaInsightsExtension:53"]
   source_code_hash = filebase64sha256(data.archive_file.lambda_zip.output_path)
   runtime          = var.lambda.runtime
 }
